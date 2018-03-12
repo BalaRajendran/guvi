a6e5ecd963fb82905515a710001d5e60b362ba6c
BalaRajendran <31990008+BalaRajendran@users.noreply.github.com>
2018-03-12 21:07:20 +0530

    Create repeat.c

---
+++ repeat.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+long int c[100000];
+int main()
+{
+	long int arr[100000],N,i;
+	int f=0;
+	scanf("%ld",&N);
+	for(i=0;i<N;i++)
+	{
+		scanf("%ld",&arr[i]);
+			c[arr[i]]++;
+	
+	}
+	for( i=0;i<N;i++)
+	{
+			if(c[i]>1)
+			{
+			f=1;
+			printf(" %ld ",i);
+			}
+  }
+  return 0;
+}
