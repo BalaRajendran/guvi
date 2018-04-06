c5c183a67f5cf456e5e8c224244f47c939648c09
BalaRajendran <31990008+BalaRajendran@users.noreply.github.com>
2018-04-06 13:39:23 +0530

    Create camel.py

---
+++ camel.py
@@ -0,0 +1,12 @@
+a=str(input())
+b=str(input())
+for i in range (len(a)):
+    if(i==int(0)):
+        print(a[i].upper(),end="")
+    else:
+        print(a[i].lower(),end="")
+for i in range (len(b)):
+    if(i==int(0)):
+        print(b[i].upper(),end="")
+    else:
+        print(b[i].lower(),end="")
